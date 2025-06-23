7ae2316b967f6cae9257ecd23ac9a835a7957608
Axel <axelmarroquinn@gmail.com>
2025-06-23 00:51:55 -0600

    Se agrega el contenido de InstruccionAnidada a la Seccion 4.
    Se agrega el contenido de InstruccionAnidada a la Seccion 4 utilizando ejemplos con for.

---
+++ Seccion4/InstruccionAnidada/InstruccionesAnidadas.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+namespace InstruccionAnidada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("*** INSTRUCCIONES ANIDADAS ***");
+
+            int i, j, resultado;
+
+            for (i = 1; i <= 10; i++)
+            {
+                WriteLine($"Tabla de multiplicar del {i}");
+
+                for (j = 1; j <= 10; j++)
+                {
+                    resultado = i * j;
+                    WriteLine($"El resultado de {i} * {j} = {resultado}");
+                } ReadLine();
+            }
+        }
+    }
+}
