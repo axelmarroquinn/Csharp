41e987741b27ae651f4f1054842bcb6d4eb224a4
Axel <axelmarroquinn@gmail.com>
2025-06-26 00:29:02 -0600

    Se agrega el ejercicio SumaDeDigitos a la carpeta Ejercicios
    El programa se encarga de sumar los dígitos de un número entero positivo.

---
+++ Ejercicios/SumaDeDigitos/SumaDeDigitos.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+namespace SumaDeDigitos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("*** EJERCICIO: SUMAR LOS DÍGITOS DE UN ENTERO ***");
+            WriteLine("Escribe un número entero positivo: ");
+            int entrada = int.Parse(ReadLine());
+            int numero;
+
+            if (entrada < 0)
+            {
+                WriteLine("El número debe ser positivo!");
+                return;
+            }
+
+            int suma = 0; // Inicialización de variables para guardar la suma de los dígitos
+
+            while (entrada > 0) // Se ejecuta mientras el número aún tenga dígitos
+            {
+                int digito = entrada % 10;  // Obtiene el último dígito 
+                suma += digito; // Suma el dígito a la variable suma
+                entrada /= 10;   // Elimina el último dígito dividiendo entre 10            
+            }
+
+            WriteLine($"La suma de los dígitos es: {suma}");
+            ReadKey();
+        }
+    }
+}
--- Ejercicios/SumaDeDigitos/obj/Debug/net8.0/SumaDeDigitos.AssemblyInfo.cs
+++ Ejercicios/SumaDeDigitos/obj/Debug/net8.0/SumaDeDigitos.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ContadorVocales")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("SumaDeDigitos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64545cab7c13bf515ab0bb6c1b587ad9f177ea1d")]
-[assembly: System.Reflection.AssemblyProductAttribute("ContadorVocales")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ContadorVocales")]
+[assembly: System.Reflection.AssemblyProductAttribute("SumaDeDigitos")]
+[assembly: System.Reflection.AssemblyTitleAttribute("SumaDeDigitos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
